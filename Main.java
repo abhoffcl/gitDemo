fa3b3b6cebe6c6d7d7ce4d9385f6d20fe30287f8
Abhishek <abhi19friends@gmail.com>
2024-04-04 20:29:29 +0530

    Added SIPCalculator file

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Addition"+Calculator.add(10,15));
         System.out.println("Interest is ="+interestCalculator.calculate(1000000,8,5));
+        System.out.println("SIP return = "+SIPCalculator.calculate(1000,12,12,20));
     }
 
 }
